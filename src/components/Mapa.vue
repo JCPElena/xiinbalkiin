f5b1c22541667293f43f6079bb587d8122e0e496
Elena <elenhaku185@gmail.com>
2020-09-30 00:19:12 -0500

    Visualizacion del mapa en la pagina web

---
+++ src/components/Mapa.vue
@@ -0,0 +1,45 @@
+<template>
+    <div ref="contenedorMapa" class="mapa"></div>
+</template>
+
+<script>
+import L from "leaflet";
+//esto son clases
+    export default {
+    name: "mapaComponent",
+    mounted(){
+this.init();
+    },
+
+    data: () => ({
+        mapa:null
+    }),
+
+    methods: {
+        async init(){
+            await this.pintarMapa();
+
+        },
+            pintarMapa(){
+                const contenedorMapa = this.$refs.contenedorMapa;
+                //instanciamos el mapa
+                this.mapa = L.map(contenedorMapa, {
+                  center: [18.0765757,-94.3811401],
+                  zoom: 13, 
+                });
+//le agregamos la capa personalizada del mapa leaflet
+                L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                 maxZoom: 18,
+                 }).addTo(this.mapa);
+            },
+        },
+    };
+</script>
+
+<style lang="scss" scoped>
+.mapa {
+    width: 100%;
+    height: 70vh;
+    z-index: 1;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
   <BarraNavegacion />
+  <Mapa />
   </div>
 </template>
 
 <script>
  import BarraNavegacion from "../components/BarraNavegacion";
+ import Mapa from "../components/Mapa";
   export default {
     name: "Home",
     components: {
       BarraNavegacion,
+      Mapa,
     },
   }
 </script>
