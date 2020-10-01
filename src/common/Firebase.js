a1880db3e2975119b8d0b1d523616a4221add2e1
Elena <elenhaku185@gmail.com>
2020-10-01 01:10:33 -0500

    Visualizacion de los marcadores con firebase

---
+++ src/common/Firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import "firebase/firestore";
+
+//credenciales
+const firebaseConfig = {
+    apiKey: "AIzaSyDQfDYBxTCte63b7qyaLgO6Ysh_KCaiZBw",
+    authDomain: "xiinbalkiin.firebaseapp.com",
+    databaseURL: "https://xiinbalkiin.firebaseio.com",
+    projectId: "xiinbalkiin",
+    storageBucket: "xiinbalkiin.appspot.com",
+    messagingSenderId: "280991191678",
+    appId: "1:280991191678:web:be2e59be8544e8faff3e2f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //instancia que nos permite la conexion a la base de datos.
+  const db = firebase.firestore();
+  
+   export { db };
--- src/components/Mapa.vue
+++ src/components/Mapa.vue
@@ -1,45 +1,65 @@
 <template>
-    <div ref="contenedorMapa" class="mapa"></div>
+  <div ref="contenedorMapa" class="mapa"></div>
 </template>
 
 <script>
 import L from "leaflet";
+import { db } from "../common/Firebase";
+
 //esto son clases
-    export default {
-    name: "mapaComponent",
-    mounted(){
-this.init();
+export default {
+  name: "mapaComponent",
+  mounted() {
+    this.init();
+  },
+
+  data: () => ({
+    mapa: null,
+  }),
+
+  methods: {
+    async init() {
+      await this.pintarMapa();
+      await this.obtenerMarcadores();
+      await this.pintarMarcador();
     },
+    pintarMapa() {
+      const contenedorMapa = this.$refs.contenedorMapa;
+      //instanciamos el mapa
+      this.mapa = L.map(contenedorMapa, {
+        center: [18.0765757, -94.3811401],
+        zoom: 13,
+      });
+      //le agregamos la capa personalizada del mapa leaflet
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 18,
+      }).addTo(this.mapa);
+    },
+    async obtenerMarcadores() {
+      try {
+        const response = await db.collection("estaciones").get();
 
-    data: () => ({
-        mapa:null
-    }),
+        response.docs.forEach((e) => {
+         let latitud = e.data().coordenadas.latitude;
+         let longitud = e.data().coordenadas.longitude;
 
-    methods: {
-        async init(){
-            await this.pintarMapa();
-
-        },
-            pintarMapa(){
-                const contenedorMapa = this.$refs.contenedorMapa;
-                //instanciamos el mapa
-                this.mapa = L.map(contenedorMapa, {
-                  center: [18.0765757,-94.3811401],
-                  zoom: 13, 
-                });
-//le agregamos la capa personalizada del mapa leaflet
-                L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-                 maxZoom: 18,
-                 }).addTo(this.mapa);
-            },
-        },
-    };
+        this.pintarMarcador([latitud, longitud]);
+        });
+      } catch (error) {
+        console.warn(error);
+      }
+    },
+    pintarMarcador(coordenadas){
+        L.marker(coordenadas).addTo(this.mapa);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .mapa {
-    width: 100%;
-    height: 70vh;
-    z-index: 1;
+  width: 100%;
+  height: 70vh;
+  z-index: 1;
 }
 </style>
