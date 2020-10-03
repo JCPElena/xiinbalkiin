31e33a42e15bb210add522f1176bd8a78f32a61a
Elena <elenhaku185@gmail.com>
2020-10-03 01:00:32 -0500

    Localizacion del usuario en el mapa

--- src/components/Mapa.vue
+++ src/components/Mapa.vue
@@ -19,16 +19,16 @@ export default {
 
   methods: {
     async init() {
+      await this.obtenerPosicion();
       await this.pintarMapa();
       await this.obtenerMarcadores();
       await this.pintarRuta();
-
     },
     pintarMapa() {
       const contenedorMapa = this.$refs.contenedorMapa;
       //instanciamos el mapa
       this.mapa = L.map(contenedorMapa, {
-        center: [18.0765757,-94.3811401],
+        center: [18.0765757, -94.3811401],
         zoom: 12,
       });
       //le agregamos la capa personalizada del mapa leaflet
@@ -41,25 +41,54 @@ export default {
         const response = await db.collection("estaciones").get();
 
         response.docs.forEach((e) => {
-         let latitud = e.data().coordenadas.latitude;
-         let longitud = e.data().coordenadas.longitude;
+          let latitud = e.data().coordenadas.latitude;
+          let longitud = e.data().coordenadas.longitude;
 
-        this.pintarMarcador([latitud, longitud]);
+          this.pintarMarcador([latitud, longitud]);
         });
       } catch (error) {
         console.warn(error);
       }
     },
-    pintarMarcador(coordenadas){
-        L.marker(coordenadas).addTo(this.mapa);
+    pintarMarcador(
+      coordenadas,
+      avatar = "https://firebasestorage.googleapis.com/v0/b/xiinbalkiin-4b927.appspot.com/o/markers%2Fparada-de-autobus.svg?alt=media&token=9142217d-914c-4e55-8206-dceeab2490ba"
+    ) {
+      let myIcon = L.icon({
+        iconUrl: avatar,
+        iconSize: [38, 95],
+        popupAnchor: [-3, -76],
+      });
+
+      L.marker(coordenadas, { icon: myIcon }).addTo(this.mapa);
     },
-    pintarRuta(){
+    pintarRuta() {
       L.geoJSON(ruta, {
         style: {
           color: "#6200EA",
           weight: 12,
         },
       }).addTo(this.mapa);
+    },
+    obtenerPosicion() {
+// Actualizar para hacer mas precisa la posicion 
+// y actualizar el marcador cuando cambie las coordenadas
+
+
+      if (!navigator.geolocation) {
+        alert("No tienes GPS");
+        return true; //mejorar el resultado
+      }
+
+      navigator.geolocation.getCurrentPosition((coordenadas) => {
+        let latitud = coordenadas.coords.latitude;
+        let longitud = coordenadas.coords.longitude;
+
+        this.pintarMarcador(
+          [latitud, longitud],
+          "https://firebasestorage.googleapis.com/v0/b/xiinbalkiin-4b927.appspot.com/o/markers%2Fpersona.svg?alt=media&token=8e15f569-a31b-409b-95c3-3791824978d4"
+        );
+      });
     },
   },
 };
